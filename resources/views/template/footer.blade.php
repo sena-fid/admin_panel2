0c3b21b3927bdda0538f1335aaae82cd3f152749
sena-fid <senafdn@icloud.com>
2022-08-31 11:02:23 +0300

    added logout button

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+    <div class="text-center">
+        2022 - Sena Fidan
+        <a href="{{ route('dashboard') }}" class="go-top">
+            <i class="fa fa-angle-up"></i>
+        </a>
+    </div>
+</footer>
